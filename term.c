1b36009bc359ed8cb52c2629b64243aee88196ae
yahei <yahei1423@gmail.com>
2025-06-16 21:55:20 +0900

    制御コード
    未対応の制御コードを受け取ったとき、その制御コードの内容を表示するようにした

--- term.c
+++ term.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -13,10 +14,14 @@
  * 疑似端末とログを管理する
  */
 
-static const char *procNCCs(Term *, const char *);
-static const char *procCC(Term *, const char *);
-
 #define READ_SIZE 16
+
+static const char *procNCCs(Term *, const char *);
+static const char *procCC(Term *, const char *, const char *);
+static const char *procESC(Term *, const char *, const char *);
+static const char *procCSI(Term *, const char *, const char *);
+static const char *procCStr(Term *, const char *, const char *);
+static const char *procSOS(Term *, const char *, const char *);
 
 Term *
 openTerm(void)
@@ -93,6 +98,7 @@ closeTerm(Term *term)
 		freeLine(term->lines[i]);
 		term->lines[i] = NULL;
 	}
+
 	free(term->lines);
 
 	free(term->readbuf);
@@ -124,8 +130,10 @@ readPty(Term *term)
 			continue;
 		}
 
-		rest = procCC(term, reading);
-		if (reading != rest) {
+		rest = procCC(term, reading, tail);
+		if (rest == NULL) {
+			break;
+		} else if (reading != rest) {
 			memmove(term->readbuf, rest, tail - rest + 1);
 			tail -= rest - term->readbuf;
 			reading = term->readbuf;
@@ -154,28 +162,166 @@ procNCCs(Term *term, const char *head)
 }
 
 const char *
-procCC(Term *term, const char *head)
+procCC(Term *term, const char *head, const char *tail)
 {
-	if (*head < 0 || (31 < *head && *head != 127))
+	if (!BETWEEN(*head, 0x00, 0x20) && *head != 0x7f)
 		return head;
 
+	/* C0 基本集合 */
 	switch (*head) {
-	case 0:  /* NUL */
+	case 0x00:  /* NUL */
 		break;
-	case 9:  /* HT */
+
+	case 0x09:  /* HT */
 		term->cursor += 8 - term->cursor % 8;
 		break;
-	case 10: /* LF */
+
+	case 0x0a: /* LF */
 		deleteTrail(getLine(term, 0));
 		term->lastline++;
 		putU32(getLine(term, 0), 0, (char32_t *)L"\0", 1);
 		break;
-	case 13: /* CR */
+
+	case 0x0d: /* CR */
 		term->cursor = 0;
 		break;
+
+	case 0x1b: /* ESC */
+		return procESC(term, head + 1, tail);
+
+	default: /* 未対応 */
+		fprintf(stderr, "Not Supported C0: (%#x)\n", *head);
 	}
 
 	return head + 1;
+}
+
+const char *
+procESC(Term *term, const char *head, const char *tail)
+{
+	if (head >= tail)
+		return NULL;
+
+	switch (*head) {
+	case 0x5b: /* CSI */
+		return procCSI(term, head + 1, tail);
+
+	case 0x58: /* SOS */
+		return procSOS(term, head + 1, tail);
+
+	case 0x50: /* DCS */
+	case 0x5d: /* OSC */
+	case 0x5e: /* PM */
+	case 0x5f: /* APC */
+		return procCStr(term, head, tail);
+
+	default:
+		/*
+		 * 未対応
+		 *
+		 * 0x20-0x2f   nF型
+		 * 0x30-0x3f   Fp/3Fp型 私用制御機能
+		 * 0x40-0x5f   Fe型     C1 補助集合
+		 * 0x60-0x7e   Fs型     標準単独制御機能 (standardのs?)
+		 */
+		if (BETWEEN(*head, 0x20, 0x7f))
+			fprintf(stderr, "Not Supported ESC Seq: ESC %c\n", *head);
+		else
+			fprintf(stderr, "Invalid ESC Seq: ESC(%#x)\n", *head);
+	}
+
+	return head + 1;
+}
+
+const char *
+procCSI(Term *term, const char *head, const char *tail)
+{
+	char param[tail - head + 1], *pp;
+	char inter[tail - head + 1], *pi;
+
+	/* パラメタバイト */
+	for (pp = param; BETWEEN(*head, 0x30, 0x40); head++) {
+		if (head >= tail)
+			return NULL;
+		*pp++ = *head;
+	}
+	*pp = '\0';
+
+	/* 中間バイト */
+	for (pi = inter; BETWEEN(*head, 0x20, 0x30); head++) {
+		if (head >= tail)
+			return NULL;
+		*pi++ = *head;
+	}
+	*pi = '\0';
+
+	if (head >= tail)
+		return NULL;
+
+	/* 終端バイト */
+	switch (*head) {
+	default: /* 未対応 */
+		if (BETWEEN(*head, 0x40, 0x7f))
+			fprintf(stderr, "Not Supported CSI: CSI(%s)%s%c\n",
+					param, inter, *head);
+		else
+			fprintf(stderr, "Invalid CSI: CSI(%s)%s(%#x)\n",
+					param, inter, *head);
+	}
+
+	return head + 1;
+}
+
+const char *
+procSOS(Term *term, const char *head, const char *tail)
+{
+	const char *ps, *ps2;
+
+	/* ST (ESC \) が出てくるまで継続 */
+	for (ps = head; !(*(ps - 1) == 0x1b && *ps == 0x5c); ps++) {
+		if (ps >= tail)
+			return NULL;
+	}
+
+	/* 未対応 */
+	fprintf(stderr, "Not Supported SOS: SOS ");
+	for (ps2 = head; ps2 <= ps; ps2++) {
+		if (BETWEEN(*ps2, 0x20, 0x7f))
+			fprintf(stderr, "%c", *ps2);
+		else
+			fprintf(stderr, "(%#x)", *ps2);
+	}
+	fprintf(stderr, "\n");
+
+	return ps + 1;
+}
+
+const char *
+procCStr(Term *term, const char *head, const char *tail)
+{
+	const char *ps, *ps2;
+
+	/* ST (ESC \) が出てくるまで継続 */
+	for (ps = head; !(*(ps - 1) == 0x1b && *ps == 0x5c); ps++) {
+		if (ps >= tail)
+			return NULL;
+
+		/* 指令列に使えない文字が出たら終了 */
+		if (!BETWEEN(*ps, 0x20, 0x7f))
+			break;
+	}
+
+	/* 未対応 */
+	fprintf(stderr, "Not Supported CtrlStr: ESC ");
+	for (ps2 = head; ps2 <= ps; ps2++) {
+		if (BETWEEN(*ps2, 0x20, 0x7f))
+			fprintf(stderr, "%c", *ps2);
+		else
+			fprintf(stderr, "(%#x)", *ps2);
+	}
+	fprintf(stderr, "\n");
+
+	return ps + 1;
 }
 
 ssize_t
--- util.h
+++ util.h
@@ -3,6 +3,7 @@
 
 #define MIN(a,b)        ((a) < (b) ? (a) : (b))
 #define MAX(a,b)        ((a) > (b) ? (a) : (b))
+#define BETWEEN(a,b,c)  ((b) <= (a) && (a) < (c))
 
 void errExit(const char *);
 void fatal(const char *);
